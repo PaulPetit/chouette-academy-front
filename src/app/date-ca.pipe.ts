8d2b928646a4e8b13d0d293ac92bbdbafacde586
Paul Petit <paul.petit76@gmail.com>
2020-03-20 12:22:04 +0100

    Mise à jour du pipe de date, renvoie N/A si le timestamp est null

--- src/app/date-ca.pipe.ts
+++ src/app/date-ca.pipe.ts
@@ -7,20 +7,24 @@ export class DateCAPipe implements PipeTransform {
 
     transform(timeStamp: number): string {
 
-        const timeZoneOffset = new Date().getTimezoneOffset();
-        const date = new Date(timeStamp * 1000);
+        if (timeStamp !== null) {
 
-        const h = date.getUTCHours() - Math.floor(timeZoneOffset / 60);
-        const m = date.getUTCMinutes() - timeZoneOffset % 60;
-        const jour = date.toISOString().substring(0, 10);
+            const date = new Date(timeStamp * 1000);
 
-        const split = jour.split('-');
+            return date.getDate().toString().padStart(2, '0')
+                + '/' +
+                (date.getMonth() + 1).toString().padStart(2, '0')
+                + '/' +
+                date.getFullYear()
+                + ' ' +
+                date.getHours().toString().padStart(2, '0')
+                + ':' +
+                date.getMinutes().toString().padStart(2, '0');
 
-        const outputDate = date.getDate().toString().padStart(2, '0') + '/' + (date.getMonth()+1).toString().padStart(2, '0') + '/' + date.getFullYear() + ' ' + date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0');
+        } else {
+            return 'N/A';
+        }
 
-
-        //return split[2] + '/' + split[1] + '/' + split[0] + " " + h.toString().padStart(2, '0') + ':' + m.toString().padStart(2, '0');
-        return outputDate;
     }
 
 }
